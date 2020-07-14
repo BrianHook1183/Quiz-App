d3f733a60f985f9779df8bfae6b28f6e5ecfb86d
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-07-14 11:30:08 -0500

    user stories added to js comments

---
+++ quiz.js
@@ -0,0 +1,17 @@
+// User needs to be able to click start button to initiate quiz.
+
+//quiz view needs to initially show that user is on 1 / x questions, and their score so far is 0. 
+
+// user needs to be able to choose one option only, and then click submit.
+
+// on submit, apropriate message of "correct" or "wrong" needs to be displayed on the same page
+
+// still on same page, correct answer should be changed to green text, and if a wrong answer was submitted, that needs to be changed to red text color . 
+
+//still on same page, aggregate score should be updated, and question number should remain the same
+
+//still on same page, Submit button needs to change into a Next button
+
+//on Next click, next question needs to populate with the correct question number/progress, as well as reset result message od correct/wrong
+
+//on submitting the last question, user needs to be led to results view, whiich shows total number correct out of total questions, with percentage correct. Button should be to restart which is indentical to the start button. 
