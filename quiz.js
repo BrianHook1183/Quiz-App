728c6bd17cd5575a11e85a1876e981e295fd5674
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-07-18 17:28:02 -0500

    STORE restructured | correct and wrong evaluation almost working

--- quiz.js
+++ quiz.js
@@ -8,10 +8,8 @@
 //TO DO: 
 // 2) figure out better way of not-rerendering html after each question
 // 3) score++ should happen as an independent function so it updates on stats bar before moving to next questions
-// 4) bug on 2nd+ times through, counter is off. (check console)
 // 5) when f/2 is selected, 2 and 2.8 both get highlighted as ther wrong answer. Its because i'm using .contains(). need to find alternative. i think its .filter
 // 6) implement A11y, (tab is not working to go through options)
-// 7) you can dismiss no sleection method and still move on. 
 
 
 const STORE = [
@@ -19,55 +17,56 @@ const STORE = [
     {
       question: "What is a prime lens?",
       options: [
-        "The first lens you buy", 
-        "A fixed focal length lens", 
-        "The lens that originally comes with a camera body as part of a kit", 
-        "A zoom lens that only covers the most used focal lengths"
+        {option: "The first lens you buy", isAnswer: false}, 
+        {option: "A fixed focal length lens", isAnswer: true}, 
+        {option: "The lens that originally comes with a camera body as part of a kit", isAnswer: false}, 
+        {option: "A zoom lens that only covers the most used focal lengths", isAnswer: false}
       ],
-      answer: "A fixed focal length lens"
+      // answer: "A fixed focal length lens"
     },
     //2
     {
       question: "Which of these is not a common f-number?",
       options: [
-        "2",
-        "2.8", 
-        "6",
-        "8"
+        {option: "2", isAnswer: false}, 
+        {option: "2.8", isAnswer: false}, 
+        {option: "6", isAnswer: true}, 
+        {option: "8", isAnswer: false}
       ],
-      answer: "6"
+      // answer: "6"
     },
     //3
     {
       question: "What is the Sunny 16 Rule?",
       options: [
-        "A method to determine the hours of usable daylight left", 
-        "A method to measure focal length by using a sundial", 
-        "A reminder to take at least 16 pictures when shooting towards harsh sunlight", 
-        "A method of estimating correct daylight exposures"
+        {option: "A method of estimating correct daylight exposures", isAnswer: true},
+        {option: "A method to determine the hours of usable daylight left", isAnswer: false}, 
+        {option: "A method to measure focal length by using a sundial", isAnswer: false}, 
+        {option: "A reminder to take at least 16 pictures when shooting towards harsh sunlight", isAnswer: false}, 
       ],
-      answer: "A method of estimating correct daylight exposures"
+      // answer: "A method of estimating correct daylight exposures"
     },
     //4
     {
-      question: "What does ISO stand for?", 
+      question: "What does ISO stand for?",
       options: [
-      "Intentional Saturation Overexposure", 
-      "Image Shutter Open", 
-      "International Sensitivity Omnipotence", 
-      "None of the above"],
-      answer: "None of the above"
+        {option: "Intentional Saturation Overexposure", isAnswer: false}, 
+        {option: "Image Shutter Open", isAnswer: false}, 
+        {option: "International Sensitivity Omnipotence", isAnswer: false}, 
+        {option: "None of the above", isAnswer: true}
+      ],
+      // answer: "None of the above",
     },
     //5
     {
       question: "What period of daytime is best described by 'golden hour'?",
       options: [
-        "The time right after sunrise", 
-        "The time right before sunset", 
-        "The time right around midday", 
-        "Both A and B"
+        {option: "The time right after sunrise", isAnswer: false}, 
+        {option: "The time right before sunset", isAnswer: false}, 
+        {option: "The time right around midday", isAnswer: false},
+        {option: "Both A and B", isAnswer: true}
       ],
-      answer: "Both A and B"
+      // answer: "Both A and B"
     },
 ];
 
@@ -77,6 +76,18 @@ let score = 0;
 let currentQuestion = 0;
 // static:
 let totalQuestions = STORE.length;
+
+// tip from a.byrd, make sure this only runs once, assign all listeners. Get in practice of doing this, if you only want a listener to run once, then it can't be embedded in functions that run multiple times. Or else my "next" button will have to listeners on it after quiz restart and questions start getting skipped. 
+// listens to "start" and "restart" buttons being clicked, listens to next button also. 
+function registerEvents() {
+  $('body').on('click','.start-button', function(event) {
+    startNewQuiz();
+  });
+  $('main').on('click', '.js-next-question', event => {
+    console.log('next button clicked');
+    nextStep();
+  })
+}
 
 
 
@@ -94,7 +105,6 @@ function startNewQuiz() {
   renderQuestionPageHtml();
   renderNextQuestion();
   handleSubmitButton();
-  handleNextButton();
 }
 
 function clearCounterVariables() {
@@ -144,12 +154,16 @@ function renderNextQuestion() {
 
 function renderOptions() {
   let question= STORE[currentQuestion];
-  for(let i=0; i < question.options.length; i++)
+  for (let i=0; i < question.options.length; i++)
   {
     $('.js-options').append(`
-        <input type = "radio" name="options" id="option${i+1}" value= "${question.options[i]}" tabindex ="${i+1}"> 
-        <label for="option${i+1}"> ${question.options[i]}</label><br/>`
+        <input type = "radio" name="options" id="option${i+1}" value= "${question.options[i].option}" data-index-number=${i} tabindex ="${i+1}"> 
+        <label data-index-number=${i} for="option${i+1}"> ${question.options[i].option}</label><br/>`
     );
+    // finds the index of the correct answer
+    if (question.options[i].isAnswer === true) {
+      realAnswerIndex = i;
+    }
   }
 }
 
@@ -157,18 +171,19 @@ function handleSubmitButton() {
   $('main').on('click', '.js-submit-answer', event => {
     event.preventDefault();
     console.log('handleSubmitButton ran');
+    const userAnswerIndex = $("input[name=options]:checked").data("index-number");
+    const userAnswerBool = STORE[currentQuestion].options[userAnswerIndex].isAnswer;
     const userAnswer = $("input[name=options]:checked").val();
-    const realAnswer = STORE[currentQuestion].answer;
-    console.log('the users answer is '+userAnswer);
-    $(".js-next-question").show();
+    console.log('the users answer is '+ userAnswerBool);
+    $(".js-next-question").show(); // show next button only after if alert has been satisfied
     $(".js-submit-answer").hide();
-    if (!userAnswer) {
-      alert("You must select your best guess before moving on!");
-      return;
-    }
-    if (userAnswer === realAnswer){
+        if (!userAnswer) {
+          alert("You must select your best guess before moving on!");
+          return;
+        }
+    if (userAnswerBool){
       // correctAnswer(); // this function used to be seperate. included here because of variable difficulty. can still move back outside later
-      console.log('correctAnswer ran and the userAnswer was ' + userAnswer);
+      console.log('correctAnswer ran');
       score++; // should probably move this into its own updateScore function so it happens live on submit instead of waiting until next screen
       // make text green for correct answer by inserting class
       $('label:contains(' +  userAnswer + ')').addClass('correct-answer');
@@ -177,25 +192,18 @@ function handleSubmitButton() {
       // to do: image
     } else {
       // wrongAnswer(); // this function used to be seperate. included here because of variable difficulty. can still move back outside later
-      console.log('wrongAnswer ran');
+      console.log('wrongAnswer ran. and the correct answer index is: ' + realAnswerIndex);
       // make selected wrong answer red
       $('label:contains(' +  userAnswer + ')').addClass('wrong-answer')
-      $('label:contains(' +  realAnswer + ')').addClass('correct-answer');
+      $('label[data-index-number=' + realAnswerIndex +']').addClass('correct-answer');
       // show "wrong" message" and correct answer
       $('.js-feedback').html(`
         <h2>Wronggggg</h2>
-        <p>the answer is "${realAnswer}"<p>
+        <p>the answer is ""<p>
       `);
       // to do: image
     }
   })
-}
-
-function handleNextButton() {
-  $('main').on('click', '.js-next-question', event => {
-    console.log('next button clicked');
-    nextStep();
-  });
 }
 
 function nextStep() { 
@@ -236,7 +244,7 @@ function renderResultsPageHtml() {
 // runs all listener functions
 function initiateQuizApp() {
   console.log('initiateQuiz ran');
-  handleStartClick();
+  registerEvents(); // contains listeners so they only run once
 }
 
 $(initiateQuizApp);
